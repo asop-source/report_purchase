0220b7bd908a9656dc3b86820d1e5681174b01cb
Asop-source <asopkarawang@gmail.com>
2019-12-23 14:04:14 +0700

    update report purchase

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Purchase",
+
+    'summary': """
+        Template Report Custom Inherit
+        """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "asopkarawang@gmail.com",
+
+    'category': 'Report',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'views/purchase.xml',
+        # 'report/purchase.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ model/purchase.py
@@ -0,0 +1,8 @@
+
+# from odoo import models, fields, api
+
+# class purchase(models.Model):
+# 	_name = 'purchase.order.line'
+# 	_inherit = 'purchase.order.line'
+
+#  nomor = fields.Chart(string="No")
